9df21c2f468f6b03ad178cf1f4dfc5b85a954cf7
VB2007 <admin@vb2007.hu>
2026-04-19 18:44:56 +0200

    refined final solution for drop_test task

--- drop_test/main.py
+++ drop_test/main.py
@@ -3,18 +3,23 @@ from pathlib import Path
 
 
 def min_number_of_drops(n: int, h: int) -> int:
+    # 1 eszköz = minden emeletet egyenként le kell tesztelni
+    if n == 1:
+        return h
+
+    # ha van elég eszköz, csak ellenőrizzük binary search-el
     if n >= ceil(log2(h + 1)):
         return ceil(log2(h + 1))
 
-    # dp[drops][devices]
-    dp = [[0] * (n + 1) for _ in range(h + 1)]
+    # dp = `m` dobás, `i` eszköz
+    dp = [0] * (n + 1)
 
     drops = 0
-    while dp[drops][n] < h:
+    while dp[n] < h:
         drops += 1
-        for devices in range(1, n + 1):
-            # ellenőrizhető szintek -> szintek alatta (törik) + szintek felette (nem törik) + 1 (jelenlegi szint)
-            dp[drops][devices] = dp[drops - 1][devices - 1] + dp[drops - 1][devices] + 1
+        # előző dobás értékeinek használata (D-1)
+        for i in range(n, 0, -1):
+            dp[i] = dp[i - 1] + dp[i] + 1
 
     return drops
 
