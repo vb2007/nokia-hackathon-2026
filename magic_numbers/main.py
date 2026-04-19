eacaaa323e03ac07f40ab982dad1d4c6187f7d37
VB2007 <admin@vb2007.hu>
2026-04-19 17:53:30 +0200

    added input parsing & types to functions

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,24 +1,34 @@
 from pathlib import Path
 
 
-def next_magic_number(n):
+def parse_input(line: str) -> int:
+    line = line.strip()
+
+    if "^" in line:  # hatványozás kezelése
+        base, exp = line.split("^")
+        return int(base) ** int(exp)
+    return int(line)
+
+
+def next_magic_number(n: int) -> int:
     n += 1
 
     while True:
-        s = str(n)
+        s = str(n)  # strnggé alakítás a fordított érték ellenőrzéséhez
+
         if s == s[::-1]:
-            return n
-        else:
-            n += 1
+            return int(n)
+        n += 1
 
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
 
     for line in data.splitlines():
-        n = int(line.strip())
-        magic_number = next_magic_number(n)
-        print(magic_number, end="")
+        number = parse_input(line)
+        magic_number = next_magic_number(number)
+
+        print(magic_number)
 
 
 if __name__ == "__main__":
