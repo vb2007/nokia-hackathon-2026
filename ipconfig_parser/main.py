91d4fb3693372fdf3a339ac3051604bc4678ed8c
VB2007 <admin@vb2007.hu>
2026-04-19 18:56:15 +0200

    implemented function for formatting data lines

--- ipconfig_parser/main.py
+++ ipconfig_parser/main.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 
 
+def format_line(line: str) -> str:
+    formatted_line = line.replace(".", "").strip().lower()
+    return formatted_line.replace(" ", "_").replace("-", "_")
+
+
 def main():
     for path in sorted(Path(".").glob("*.txt")):
         print(path.name)
