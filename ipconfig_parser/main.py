06dea06fab5351568a87000716c3110ecea689d1
VB2007 <admin@vb2007.hu>
2026-04-19 19:05:27 +0200

    implemented file writing in main() function, added types to ipconfig_parser function

--- ipconfig_parser/main.py
+++ ipconfig_parser/main.py
@@ -1,14 +1,28 @@
+from json import dump
 from pathlib import Path
-
 
 def format_line(line: str) -> str:
     formatted_line = line.replace(".", "").strip().lower()
     return formatted_line.replace(" ", "_").replace("-", "_")
 
+def ipconfig_parser(file_name: str, lines: list[str]) -> dict:
+    output = []
+
+    for path
 
 def main():
+    final_output = []
+
     for path in sorted(Path(".").glob("*.txt")):
-        print(path.name)
+        with open(path, "r", encoding="utf-8", errors="replace") as file:
+            lines = file.readlines()
+
+        parsed_data = ipconfig_parser(path.name, lines)
+        if parsed_data:
+            final_output.append(parsed_data)
+
+    with open("output_simple.json", "w", encoding="utf-8") as file:
+        dump(final_output, file, indent=4)
 
 
 if __name__ == "__main__":
