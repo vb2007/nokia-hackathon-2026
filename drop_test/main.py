8d75488529d3ef31176694a2cb4bc619832e5258
VB2007 <admin@vb2007.hu>
2026-04-19 18:37:53 +0200

    created POC solution for task drop_test

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,8 +1,36 @@
 from pathlib import Path
 
 
+def min_number_of_drops(n: int, h: int) -> int:
+    # dp[devices][floors]
+    dp = [[0] * (h + 1) for _ in range(n + 1)]
+
+    for j in range(1, h + 1):
+        dp[1][j] = j
+
+    for i in range(2, n + 1):
+        for j in range(1, h + 1):
+            dp[i][j] = float("inf")
+            for x in range(1, j + 1):
+                breaks = dp[i - 1][x - 1]
+                survives = dp[i][j - x]
+
+                worst_case = 1 + max(breaks, survives)
+
+                if worst_case < dp[i][j]:
+                    dp[i][j] = worst_case
+
+    return dp[n][h]
+
+
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
+
+    for line in data.splitlines():
+        n, h = line.split()
+        result = min_number_of_drops(int(n), int(h))
+        print(result)
+
     print(data, end="")
 
 
